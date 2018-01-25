645ae8a10bfb7e37b1ae0a89e884b34c4bc9c170
mkg20001 <mkg20001@gmail.com>
2018-01-25 14:12:40 +0100

    Fix dummy test
    Ugh...

--- test/dummy.spec.js
+++ test/dummy.spec.js
@@ -1,1 +1,1 @@
-it('tests are in the https://github.com/libp2p/js-libp2p-websocket-star-rendezvous repo! Please clone that and link the package!')
+it('tests are in the https://github.com/libp2p/js-libp2p-websocket-star-rendezvous repo! Please clone that and link the package!', () => {})
